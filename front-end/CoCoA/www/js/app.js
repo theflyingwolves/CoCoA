daa677aad3a1cf77a0472d8b4dc6e3e72c2d0e60
Wang Kunzhen <theflyingwolves@gmail.com>
2014-10-15 21:51:41 +0800

    Project framework Initialized

--- front-end/CoCoA/www/js/app.js
+++ front-end/CoCoA/www/js/app.js
@@ -1,10 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('cocoa', ['ionic', 'cocoa.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,47 +20,20 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     .state('app', {
       url: "/app",
       abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
+      templateUrl: "templates/userGropuMenu.html",
+      controller: 'usergroupCtrl'
     })
 
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
+    .state('app.eventview',{
+      url:"/eventview",
+      views:{
+        content:{
+          templateUrl:"templates/eventlist.html",
+          controller:"eventlistCtrl"
         }
       }
     })
-
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
-    });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  
+  $urlRouterProvider.otherwise('/app/eventview');
 });
 
--- front-end/CoCoA/www/js/controllers.js
+++ front-end/CoCoA/www/js/controllers.js
@@ -1,48 +1,92 @@
-angular.module('starter.controllers', [])
+angular.module('cocoa.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+.factory('Usergroups',function(){
+  return {
+    all: function(){
+      var allgroups = window.localStorage['usergroups'];
+      if(allgroups){
+        return angular.fromJson(allgroups);
+      }else{
+        return [];
+      }
+    },
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+    save: function(projects){
+      window.localStorage['usergroups'] = angular.toJson(projects);
+    },
+
+    newCCA: function(name){
+      return {
+        title:name,
+        events:[]
+      };
+    },
+
+    getLastActiveIndex: function(){
+      return parseInt(window.localStorage['lastActiveProject']) || 0;
+    },
+
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveProject'] = index;
+    }
+  };
+})
+
+.controller('usergroupCtrl',function($scope,$timeout, $ionicSideMenuDelegate, Usergroups){
+  $scope.usergroups = Usergroups.all();
+  $scope.activeGroup = $scope.usergroups[Usergroups.getLastActiveIndex()];
+  var createNewCCA = function(name){
+    var newCCA = Usergroups.newCCA(name);
+    $scope.usergroups.push(newCCA);
+    Usergroups.save($scope.usergroups);
+    $scope.selectCCA(newCCA, $scope.usergroups.length - 1);
+  };
+
+  $scope.newUserGroup = function(){
+    var ccaName = promp("Name for new CCA");
+    if(ccaName){
+      createNewCCA(ccaName);
+    }
+  };
+
+  $scope.selectCCA = function(cca, index){
+    $scope.activeGroup = cca;
+    Usergroups.setLastActiveIndex(index);
+    $ionicSideMenuDelegate.toggleLeft(false);
+  };
+
+  $timeout(function() {
+    if($scope.usergroups.length == 0) {
+      while(true) {
+        var ccaName = prompt('Your first CCA name:');
+        if(ccaName) {
+          createNewCCA(ccaName);
+          break;
+        }
+      }
+    }
   });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+.controller('eventlistCtrl',function($scope){
+  $scope.eventlist = [
+  {
+    id:'event1',
+    title:'Event 1'
+  },
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+  {
+    id:'event2',
+    title:'Event 2'
+  },
+
+  {
+    id:'event3',
+    title:'Event 3'
+  },
+
+  {
+    id:'event4',
+    title:'Event 4'
+  }];
+})
