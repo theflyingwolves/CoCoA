77a08296548d5438c571cb9c151fcf6e4edb3c7d
Li Zhenshuo <pleasenice@gmail.com>
2014-10-26 23:41:36 +0800

    use search function

--- CoCoA-backend/app.js
+++ CoCoA-backend/app.js
@@ -16,6 +16,8 @@ cookieParser = require('cookie-parser'),
 
 methodOverride = require('method-override'),
 
+async = require('async'),
+
 gapi = require('./lib/gapi');
 var googleSpreadsheet = require('edit-google-spreadsheet');
 
@@ -45,33 +47,35 @@ app.get('/', function(req, res) {
     res.render('index.jade', locals);
 });
 
-app.get('/oauth2callback', function(req, res) {
-    var code = req.query.code;
+app.get('/oauth2callback', function(request, response) {
+    var code = request.query.code;
     gapi.oauth2Client.getToken(code, function(err, tokens){
         gapi.oauth2Client.setCredentials(tokens);
-        gapi.googleDrive.files.list({'access_token':gapi.oauth2Client.credentials.access_token}, function(req,res){
-            var list = res;
-           for(var i = 0;i<list.items.length;i++){
-                if(list.items[i].title == 'CCA-Admin' && list.items[i].mimeType == 'application/vnd.google-apps.folder'){
-                    console.log("root file's id is "+list.items[i].id);
-                    rootFolderId = list.items[i].id;
-                }
+        gapi.googleDrive.files.list({
+            'access_token':gapi.oauth2Client.credentials.access_token,
+            'q':"title = 'CCA-Admin'"
+        }, function(err,res){
+            if(res.items.length == 1){
+                rootFolderId = res.items[0].id;
+                console.log("root file's id is "+rootFolderId);
+            }
+            else{
+                response.send("You have more than one CCA-Admin Folder");
             }
         });
     });
-    
     var locals = {
         title: 'What are you doing?',
         url: gapi.authUrl
     };
-    res.render('index.jade', locals);
+    response.render('index.jade', locals);
 });
 
-app.post('/create-cca', function(req,res){
+app.post('/cca', function(request,response){
     console.log("fetch root folder id in create-cca: "+rootFolderId);
     gapi.googleDrive.files.insert({
         resource: {
-            title: req.body.cca_title,
+            title: request.body.cca_title,
             mimeType: 'application/vnd.google-apps.folder',
             parents: [{
             "kind":"drive#fileLink",
@@ -79,7 +83,7 @@ app.post('/create-cca', function(req,res){
             }]
         }
     },
-        function(req,res){
+        function(err,res){
             gapi.googleDrive.files.insert({
                 resource: {
                     title: res.title+"-events",
@@ -96,7 +100,7 @@ app.post('/create-cca', function(req,res){
 
             gapi.googleDrive.files.insert({
                 resource: {
-                    title: res.title+" - CCA Record",
+                    title: res.title+"-CCA Record",
                     mimeType: 'application/vnd.google-apps.spreadsheet',
                     parents: [
                     {
@@ -126,7 +130,7 @@ app.post('/create-cca', function(req,res){
 
             gapi.googleDrive.files.insert({
                 resource: {
-                    title: res.title+" - List of Events",
+                    title: res.title+"-List of Events",
                     mimeType: 'application/vnd.google-apps.spreadsheet',
                     parents: [
                     {
@@ -158,7 +162,7 @@ app.post('/create-cca', function(req,res){
 
             gapi.googleDrive.files.insert({
                 resource: {
-                    title: res.title+" - Student Details",
+                    title: res.title+"-Student Details",
                     mimeType: 'application/vnd.google-apps.spreadsheet',
                     parents: [
                     {
@@ -188,7 +192,60 @@ app.post('/create-cca', function(req,res){
             });
             console.log("create a new folder under CCA-Admin");
     });
-    res.send("successful");
+    response.send("successful create cca");
+});
+
+app.get('/cca', function(request,response){
+    var ccaList = [];
+    gapi.googleDrive.files.list({
+        'access_token':gapi.oauth2Client.credentials.access_token,
+        'q':"'"+rootFolderId+"' in parents"
+    }, function(err,res){
+        async.each(res.items, function(item,callback){
+            var cca = {};
+            cca.title = item.title;
+            cca.id = item.id;
+            ccaList.push(cca);
+            callback();
+        }, function(err){
+            if(err){
+                console.log(err);
+            } else{
+                response.send(JSON.stringify(ccaList,null,4));
+            }
+        });
+    });
+});
+
+app.post('/cca/:id/events', function(request,response){
+    gapi.googleDrive.files.insert({
+        resource: {
+            title: request.body.event_title,
+            mimeType: 'application/vnd.google-apps.spreadsheet',
+            parents: [{
+            "kind":"drive#fileLink",
+            "id":request.body.event_folder_id
+            }]
+        }
+    },  function(err,res){
+            googleSpreadsheet.load({
+                debug:true,
+                spreadsheetId:res.id,
+                worksheetId:'od6',
+                accessToken:{
+                    type:'Bearer',
+                    token:gapi.oauth2Client.credentials.access_token
+                }
+            },  function sheetReady(err, spreadsheet) {
+                    if(err) throw err;
+                    spreadsheet.add([['Name of Student','ID','Level','Class']]);
+                    spreadsheet.send(function(err) {
+                    if(err) throw err;
+                    console.log("created successfully");
+                });
+            });
+        response.send({"event_title":request.body.event_title,"event_spreadsheet_id":res.id});
+    });
 });
 
 var server = app.listen(3000);
